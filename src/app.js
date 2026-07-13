e536ff766d85049fce6f21ec4e39c013d4321666
Farhan-56-Amd <farhan76754@gmail.com>
2026-07-13 21:31:36 +0530

    Crud on User Data

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 
+// Sign Up API
 app.post('/signup',async (req,res) => {
   try {
       const userData = req.body
@@ -13,9 +14,72 @@ app.post('/signup',async (req,res) => {
   
       res.send('User is Registered successfully')
     } catch(err) {
-      res.statusCode(500).send('Something went wrong')
+      res.status(500).send('Something went wrong')
     }
 
+})
+
+// get user using emailId
+app.get('/user',async (req,res) => {
+   try {
+     const email = req.body.email
+     console.log(email)
+     const user = await User.findOne({email : email})
+  
+     if(!user) {
+        return res.status(500).send('User not found')
+     } 
+       res.send(user)
+     
+
+   }catch (err) {
+     res.status(401).send('Something went wrong!')
+   }
+})
+
+// get feed API 
+app.get('/feed',async (req,res) => {
+  try {
+     const users = await User.find({})
+
+     if(!users.length) {
+      return res.status(500).send('No user found')
+     }
+
+     res.send(users)
+  } catch (err) {
+     res.status(401).send('Something went Wrong!')
+  }
+})
+
+// delete User
+
+app.delete('/user',async (req,res) => {
+  try {
+    const userId = req.body.userId
+
+    await User.findByIdAndDelete({_id : userId})
+
+    res.send('User deleted successdfully')
+  }catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+//Update User details 
+
+app.patch('/user', async (req,res) => {
+  try {
+    console.log(req.body)
+    const userId = req.body.userId
+    const data = req.body
+    console.log(data)
+    const user = await User.findByIdAndUpdate({_id : userId},data)
+
+    res.send('User data updated successfully')
+  }catch (err) {
+    res.status(401).send(err.message)
+  }
 })
 
 connectDB()
